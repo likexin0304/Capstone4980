1d58613190f58f4ab3536808a816d44cbc247365
likexin <danielleeprince@gmail.com>
2018-04-07 12:01:30 -0500

    Firist commit

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDp2FONtpE_bnXtvtPtQMcxkQBLYtAuasQ",
+  authDomain: "capstonesp18.firebaseapp.com",
+  databaseURL: "https://capstonesp18.firebaseio.com",
+  projectId: "capstonesp18",
+  storageBucket: "capstonesp18.appspot.com",
+  messagingSenderId: "874627600816"
+};
+
